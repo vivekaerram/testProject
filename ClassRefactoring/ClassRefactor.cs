a4a7e5f49e1fe3fdfe5d8f3913067ee3553ac19d
vivekaerram <vivekaerram@gmail.com>
2023-11-18 12:13:34 +0530

    claa refactor issue resolved

--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -14,43 +14,42 @@ namespace DeveloperSample.ClassRefactoring
 
     public class SwallowFactory
     {
-        public Swallow GetSwallow(SwallowType swallowType) => new Swallow(swallowType);
+        public Swallow GetSwallow(SwallowType swallowType)
+        {
+            return swallowType switch
+            {
+                SwallowType.African => new AfricanSwallow(),
+                SwallowType.European => new EuropeanSwallow(),
+                _ => throw new ArgumentException("Invalid swallow type")
+            };
+        }
     }
 
-    public class Swallow
+    public abstract class Swallow
     {
-        public SwallowType Type { get; }
         public SwallowLoad Load { get; private set; }
-
-        public Swallow(SwallowType swallowType)
-        {
-            Type = swallowType;
-        }
 
         public void ApplyLoad(SwallowLoad load)
         {
             Load = load;
         }
 
-        public double GetAirspeedVelocity()
+        public abstract double GetAirspeedVelocity();
+    }
+
+    public class AfricanSwallow : Swallow
+    {
+        public override double GetAirspeedVelocity()
         {
-            if (Type == SwallowType.African && Load == SwallowLoad.None)
-            {
-                return 22;
-            }
-            if (Type == SwallowType.African && Load == SwallowLoad.Coconut)
-            {
-                return 18;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.None)
-            {
-                return 20;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.Coconut)
-            {
-                return 16;
-            }
-            throw new InvalidOperationException();
+            return Load == SwallowLoad.None ? 22 : 18;
+        }
+    }
+
+    public class EuropeanSwallow : Swallow
+    {
+        public override double GetAirspeedVelocity()
+        {
+            return Load == SwallowLoad.None ? 20 : 16;
         }
     }
 }
