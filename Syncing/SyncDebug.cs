67ca8bdbd83e13313cd5bb643de6a3f534e8cc63
vivekaerram <vivekaerram@gmail.com>
2023-11-18 11:13:43 +0530

    SyncDebug project completed

--- Syncing/SyncDebug.cs
+++ Syncing/SyncDebug.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeveloperSample.Syncing
@@ -12,37 +11,21 @@ namespace DeveloperSample.Syncing
         public List<string> InitializeList(IEnumerable<string> items)
         {
             var bag = new ConcurrentBag<string>();
-            Parallel.ForEach(items, async i =>
+            foreach (var item in items)
             {
-                var r = await Task.Run(() => i).ConfigureAwait(false);
-                bag.Add(r);
-            });
-            var list = bag.ToList();
-            return list;
+                bag.Add(item);
+            }
+            return bag.ToList();
         }
 
         public Dictionary<int, string> InitializeDictionary(Func<int, string> getItem)
         {
-            var itemsToInitialize = Enumerable.Range(0, 100).ToList();
-
             var concurrentDictionary = new ConcurrentDictionary<int, string>();
-            var threads = Enumerable.Range(0, 3)
-                .Select(i => new Thread(() => {
-                    foreach (var item in itemsToInitialize)
-                    {
-                        concurrentDictionary.AddOrUpdate(item, getItem, (_, s) => s);
-                    }
-                }))
-                .ToList();
 
-            foreach (var thread in threads)
+            Parallel.ForEach(Enumerable.Range(0, 100), (item) =>
             {
-                thread.Start();
-            }
-            foreach (var thread in threads)
-            {
-                thread.Join();
-            }
+                concurrentDictionary.GetOrAdd(item, getItem);
+            });
 
             return concurrentDictionary.ToDictionary(kv => kv.Key, kv => kv.Value);
         }
