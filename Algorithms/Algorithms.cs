18e52b6efc625dc13fab9d6203880428de49fd26
vivekaerram <vivekaerram@gmail.com>
2023-11-18 12:13:50 +0530

    write the given algorithms

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,43 @@
 using System;
+using System.Text;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Factorial of negative number is not defined.");
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            int factorial = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null || items.Length == 0)
+                return string.Empty;
+
+            if (items.Length == 1)
+                return items[0];
+
+            StringBuilder formattedString = new StringBuilder();
+            for (int i = 0; i < items.Length; i++)
+            {
+                formattedString.Append(items[i]);
+
+                if (i < items.Length - 2)
+                    formattedString.Append(", ");
+                else if (i == items.Length - 2)
+                    formattedString.Append(" and ");
+            }
+
+            return formattedString.ToString();
+        }
     }
 }
