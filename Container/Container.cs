9d0cd183e7b0dad86e6d50c353ef3c3bfac7b302
vivekaerram <vivekaerram@gmail.com>
2023-11-18 11:58:57 +0530

    fixed the container project

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private readonly Dictionary<Type, Type> _bindings;
+
+        public Container()
+        {
+            _bindings = new Dictionary<Type, Type>();
+        }
+
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            if (!interfaceType.IsAssignableFrom(implementationType))
+            {
+                throw new ArgumentException($"Type {implementationType.Name} must implement {interfaceType.Name}.");
+            }
+
+            _bindings[interfaceType] = implementationType;
+        }
+
+        public T Get<T>()
+        {
+            Type serviceType = typeof(T);
+
+            if (!_bindings.TryGetValue(serviceType, out Type implementationType))
+            {
+                throw new InvalidOperationException($"No binding found for type {serviceType.Name}.");
+            }
+
+            return (T)Activator.CreateInstance(implementationType);
+        }
     }
 }
--- Container/ContainerTest.cs
+++ Container/ContainerTest.cs
@@ -1,18 +1,10 @@
 using Xunit;
 
 namespace DeveloperSample.Container
-{
-    internal interface IContainerTestInterface
-    {
-    }
-
-    internal class ContainerTestClass : IContainerTestInterface
-    {
-    }
-
+{  
     public class ContainerTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanBindAndGetService()
         {
             var container = new Container();
